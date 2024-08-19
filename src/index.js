21ffc72478a91a9bb03a0474a1fc2e9d65f39d05
ForgetMeNot152 <nikitamks@yandex.ru>
2024-08-19 21:44:36 +0300

    feat: add this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(huy) {
+    if (huy < 0) {
+        huy = huy * -1;
+      }
+return Number(String(huy).split("").reverse().join(""));
 }
+console.log(module.exports(-425))
